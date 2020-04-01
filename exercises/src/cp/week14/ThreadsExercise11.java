1a492b106910ee13a72666a6c868d8cd075dda0b
Jakob Kjær-Kammersgaard <jakob@miscellus.dk>
2020-04-01 15:41:12 +0200

    week_14

--- exercises/src/cp/week14/ThreadsExercise11.java
+++ exercises/src/cp/week14/ThreadsExercise11.java
@@ -1,5 +1,19 @@
 package cp.week14;
 
+import cp.week13.Words;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
@@ -12,4 +26,105 @@ public class ThreadsExercise11
 	Which executor runs faster?
 	Can you explain why?
 	*/
+    
+    public static void main(String[] args)
+	{
+		// word -> number of times that it appears over all files
+		//Map< String, Integer > occurrences = new ConcurrentHashMap<>();
+        Map< Character, Set<String>> wordSetsPerCharacter = new ConcurrentHashMap<>();
+		
+		List< String > filenames = List.of(
+			"text1.txt",
+			"text2.txt",
+			"text3.txt",
+			"text4.txt",
+			"text5.txt",
+			"text6.txt",
+			"text7.txt",
+			"text8.txt",
+			"text9.txt",
+			"text10.txt"
+		);
+		
+        
+        long t1, t2;
+        t1 = System.currentTimeMillis();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        t2 = System.currentTimeMillis();
+        System.out.println( "Time to initialize executorService: " + (t2-t1) + "ms" );	
+        
+		filenames.stream()
+            .map( filename -> "data/" + filename )
+            .forEach(filename -> {
+                executor.submit(() -> {
+                    computeOccurrences( filename, wordSetsPerCharacter );
+                });
+            });
+        
+		try {
+			//executor.shutdownNow();
+            t1 = System.currentTimeMillis();
+            executor.shutdown();
+            executor.awaitTermination(1, TimeUnit.HOURS);
+            t2 = System.currentTimeMillis();
+		} catch( InterruptedException e ) {
+			e.printStackTrace();
+            t1 = 0;
+            t2 = 0;
+		}
+		
+        //wordSetsPerCharacter.forEach( (startingChar, setOfWordsStartingWithStartingChar) -> System.out.println( startingChar + ": " + setOfWordsStartingWithStartingChar ) );
+        System.out.println( "Elapsed time: " + (t2-t1) + "ms" );	
+//		occurrences.forEach( (word, n) -> System.out.println( word + ": " + n ) );
+	}
+	
+	private static void computeOccurrences( String filename, Map< Character, Set<String> > wordSetsPerCharacter )
+	{
+
+		try {
+			Files.lines( Paths.get( filename ) )
+				.flatMap( Words::extractWords )
+				//.map( String::toLowerCase )
+				.forEach( word -> {
+                    char startingCharacter = word.charAt(0);
+                    
+                    // This is wrong!
+//                    Set<String> initialSet = new HashSet<>();
+//                    initialSet.add(word);
+//                    
+//                    Set<String> existingSet = wordSetsPerCharacter.getOrDefault(startingCharacter, initialSet);
+//                    
+                    
+                    
+                    if (false) {
+                        Set<String> someSet = new HashSet<>();
+                        someSet.add(word);
+                        wordSetsPerCharacter.merge(startingCharacter, someSet, (existingSet, addedSet) -> {
+                           if (existingSet == null) {
+                               return addedSet;
+                           }
+                           else {
+                               existingSet.addAll(addedSet);
+                               return existingSet;
+                           }
+                        });
+                    }
+                    else {
+                        wordSetsPerCharacter.compute(startingCharacter, (unsusedKey, existingSet) -> {
+                            if (existingSet == null) {
+                               Set<String> initialSet = new HashSet<>();
+                               initialSet.add(word);
+                               return initialSet;
+                            }
+                            else {
+                                existingSet.add(word);
+                                return existingSet;
+                            }
+                        });
+                    }
+				} );
+		} catch( IOException e ) {
+			e.printStackTrace();
+		}
+	}
 }
--- exercises/src/cp/week14/ThreadsExercise13.java
+++ exercises/src/cp/week14/ThreadsExercise13.java
@@ -1,5 +1,17 @@
 package cp.week14;
 
+import cp.week13.Words;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  *
  * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
@@ -10,4 +22,78 @@ public class ThreadsExercise13
 	Modify Threads/cp/WalkExecutor such that all threads stop processing as soon as the word "nulla" is found.
 	Hint: Use an atomic boolean shared by all threads. Try not to make it a static field, but rather a local variable within the main method.
 	*/
+    public static void main(String[] args)
+	{
+		// word -> number of times it appears over all files
+		Map< String, Integer > occurrences = new ConcurrentHashMap<>();
+//		ExecutorService executor = Executors.newCachedThreadPool();
+		ExecutorService executor = Executors.newFixedThreadPool( Runtime.getRuntime().availableProcessors() );
+		
+        AtomicBoolean seenNulla = new AtomicBoolean(false);
+        
+		try {
+			Files.walk( Paths.get( "data" ) )
+				.filter( Files::isRegularFile )
+				.forEach( filepath -> {
+					executor.submit( () -> {
+						computeOccurrences( filepath, occurrences, seenNulla, executor );
+					} );
+				} );
+		} catch( IOException e ) {
+			e.printStackTrace();
+		}
+		
+		try {
+			executor.shutdown();
+			executor.awaitTermination( 1, TimeUnit.DAYS );
+		} catch( InterruptedException e ) {
+			e.printStackTrace();
+		}
+		
+		occurrences.forEach( (word, n) -> System.out.println( word + ": " + n ) );
+	}
+	
+	private static void computeOccurrences( Path textFile, Map< String, Integer > occurrences, AtomicBoolean seenNulla, ExecutorService executor )
+	{
+		try {
+			Files.lines( textFile )
+				.flatMap( Words::extractWords )
+				.map( String::toLowerCase )
+                .takeWhile( word -> {
+                    boolean isCurrentWordNulla = word.equals("nulla");                    
+                    boolean notFoundAlready = seenNulla.compareAndSet(false, isCurrentWordNulla);
+                    /*
+                    // The line,
+                    //     boolean notFoundAlready = seenNulla.compareAndSet(false, isCurrentWordNulla);
+                    // corresponds to the following code:
+                    
+                    boolean notFoundAlready;
+                    synchronized (seenNulla) {
+                        boolean oldValue = seenNulla.get();
+                        if (!oldValue) {
+                            seenNulla.set(isCurrentWordNulla);
+                        }
+                        notFoundAlready = (oldValue == false);
+                    }
+                    //*/
+                    
+                    return  !isCurrentWordNulla && notFoundAlready;
+
+                })
+				.forEach( s -> {
+                    /*if (seenNulla.get() || s.equals("nulla")) {
+                        if(!seenNulla.get()){
+                            seenNulla.set(true);
+                            occurrences.merge( s, 1, Integer::sum );
+                        }
+                        executor.shutdownNow();
+                    }
+                    else {*/
+                    occurrences.merge( s, 1, Integer::sum );
+                    //}
+                });
+		} catch( IOException e ) {
+			e.printStackTrace();
+		}
+	}
 }
